647ac69a6b1831503a003c45601d5e96d9f7389c
ultima-vox <rz300@ya.ru>
2026-09-01 22:34:05 +0300

    feat: make psy bass voice parameters controllable

--- src/dsp/PsyBassVoice.h
+++ src/dsp/PsyBassVoice.h
@@ -2,6 +2,7 @@
 #include <JuceHeader.h>
 
 namespace vstengine::dsp {
+
 class PsyBassSound final : public juce::SynthesiserSound {
 public:
     bool appliesToNote(int) override { return true; }
@@ -18,16 +19,34 @@ public:
     void renderNextBlock(juce::AudioBuffer<float>&, int startSample, int numSamples) override;
 
     void setDrive(float value) noexcept { drive = value; }
-    void setRelease(float seconds) noexcept { releaseSeconds = seconds; }
+    void setPitchEnvelope(float semitones, float seconds) noexcept;
+    void setAmpEnvelope(float attack, float decay, float sustain, float release) noexcept;
+    void setFilter(float cutoffHz, float resonanceQ);
 
 private:
+    void updateAmpEnvelope();
+
     double phase {};
     double baseFrequency {};
     float level {};
     float drive { 1.8f };
+
+    float pitchEnvSemitones { 12.0f };
+    float pitchEnvSeconds { 0.018f };
+
+    float attackSeconds { 0.001f };
+    float decaySeconds { 0.055f };
+    float sustainLevel { 0.72f };
     float releaseSeconds { 0.035f };
+
+    float cutoffHz { 1800.0f };
+    float resonanceQ { 0.8f };
+
     int pitchEnvelopeSamples {};
     int pitchEnvelopePosition {};
+
     juce::ADSR ampEnvelope;
+    juce::IIRFilter filter;
 };
+
 } // namespace vstengine::dsp
