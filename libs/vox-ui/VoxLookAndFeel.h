851ac8ff4ad5bcc67098015a73bd83737f9b7ec8
ultima-vox <rz300@ya.ru>
2026-09-14 12:45:39 +0300

    feat(ui): add reusable VOX look-and-feel interface

---
+++ libs/vox-ui/VoxLookAndFeel.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <juce_gui_basics/juce_gui_basics.h>
+
+namespace vox::ui {
+
+class VoxLookAndFeel final : public juce::LookAndFeel_V4 {
+public:
+    VoxLookAndFeel();
+
+    void drawButtonBackground (juce::Graphics&, juce::Button&,
+                               const juce::Colour&, bool isMouseOverButton,
+                               bool isButtonDown) override;
+
+    void drawRotarySlider (juce::Graphics&, int x, int y, int width, int height,
+                           float sliderPosProportional, float rotaryStartAngle,
+                           float rotaryEndAngle, juce::Slider&) override;
+
+    void drawComboBox (juce::Graphics&, int width, int height, bool isButtonDown,
+                       int buttonX, int buttonY, int buttonW, int buttonH,
+                       juce::ComboBox&) override;
+};
+
+} // namespace vox::ui
